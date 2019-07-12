4c586beb0d6402adf01e39a061810f6b96a556ad
Amzani, Samir <Samir.Amzani@adidas.com>
2019-07-12 14:26:48 +0200

    bugfix: include template, blueprint and oas file

--- commands/root.go
+++ commands/root.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"os"
 
-	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -97,15 +96,15 @@ func initConfig() {
 		viper.SetConfigFile(cfgFile)
 	} else {
 		// Find home directory.
-		home, err := homedir.Dir()
+		dir, err := os.Getwd()
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
 
 		// Search config in home directory with name ".api-gateway" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".api-blueprint")
+		viper.AddConfigPath(dir)
+		viper.SetConfigName("api-blueprint")
 	}
 
 	if err := viper.ReadInConfig(); err != nil {
